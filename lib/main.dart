b59de5a2d96b4574c2326ab5fa986861a9cb5847
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-10-25 10:58:14 +0300

    build calculatorButton widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class CalculatorButton extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+
+  const CalculatorButton(
+      {super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => onPressed(text),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.grey[300],
+        foregroundColor: Colors.black,
+        padding: const EdgeInsets.all(16.0),
+        textStyle: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ),
+      child: Text(text),
+    );
+  }
+}
